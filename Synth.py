39165cfdf0307db27ba41b7b67c5257c60f2b7a4
fevertree <marko.joh@gmail.com>
2016-03-20 17:17:24 +0200

    Added visualization option for sound waves to Synth class using matplotlib

--- Synth.py
+++ Synth.py
@@ -1,21 +1,25 @@
 import numpy as np
 import wave
 import pyaudio
+import matplotlib.pyplot as plt
 
 class Synth():
 
 	def __init__(self):
 		self.volume = 16384
 		self.samplerate = 44100
-		self.buffer = ''
 		self.p = pyaudio.PyAudio()
 		self.stream = self.p.open(format=8,channels=1,rate=self.samplerate,output=True)
 		self.notes = {"C":261.63, "D":293.66, "E":329.63, "F":349.23, "G":392.0,"A":440.0, "B":493.88}
+		self.totalsignal = []
+
+	def __enter__(self):
+		return self
 
 	def playSignal(self, signal):
 		self.stream.write(signal)
 
-	def genNote(self,note,duration,w):
+	def genNote(self,note,duration,type):
 		A = 1
 		frequency = self.notes[note]
 		samples = duration*self.samplerate
@@ -23,9 +27,9 @@ class Synth():
 		omega = np.pi * 2 / phase
 		period = np.arange(int(phase),dtype = np.float) * omega
 
-		if w == "sin":
+		if type == "sin":
 			y = A * np.sin(period)
-		if w == "saw":
+		if type == "saw":
 			y = A - ((A / np.pi) * period)
 
 		ydata = self.volume * y
@@ -33,14 +37,25 @@ class Synth():
 
 		ssignal = ''
 		for i in range(len(signal)):
+		   self.totalsignal.append(signal[i] / self.volume)
 		   ssignal += wave.struct.pack('h',signal[i])
-
+		
 		self.playSignal(ssignal)
 
-	def close(self):
+	def visualize(self):
+		l = len(self.totalsignal)
+		xLen = l
+		x = np.arange(int(xLen))
+		y = [s for s in self.totalsignal]
+		y = y[:len(x)]
+		fig = plt.figure()
+		ax = fig.add_subplot(211)
+		line1 = ax.plot(x, y)
+		plt.show()
+
+	def __exit__(self, exc_type, exc_value, traceback):
 		self.stream.stop_stream()
 		self.stream.close()
 		self.p.terminate()
-
 
 
--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from Synth import Synth
 
-syn1 = Synth()
-
-
 #sepapoisid
 
-syn1.genNote("C",0.25,"saw")
-syn1.genNote("D",0.25,"saw")
-syn1.genNote("E",0.25,"saw")
-syn1.genNote("C",0.25,"saw")
-syn1.genNote("C",0.25,"saw")
-syn1.genNote("D",0.25,"saw")
-syn1.genNote("E",0.25,"saw")
-syn1.genNote("C",0.25,"saw")
-syn1.genNote("E",0.25,"saw")
-syn1.genNote("F",0.25,"saw")
-syn1.genNote("G",0.5,"saw")
-syn1.genNote("E",0.25,"saw")
-syn1.genNote("F",0.25,"saw")
-syn1.genNote("G",0.5,"saw")
+with Synth() as syn1:
+	syn1.genNote("B",0.1,"sin")
+	syn1.visualize()
+	# syn1.genNote("D",0.25,"saw")
+	# syn1.genNote("E",0.25,"saw")
+	# syn1.genNote("C",0.25,"saw")
+	# syn1.genNote("C",0.25,"saw")
+	# syn1.genNote("D",0.25,"saw")
+	# syn1.genNote("E",0.25,"saw")
+	# syn1.genNote("C",0.25,"saw")
+	# syn1.genNote("E",0.25,"saw")
+	# syn1.genNote("F",0.25,"saw")
+	# syn1.genNote("G",0.5,"saw")
+	# syn1.genNote("E",0.25,"saw")
+	# syn1.genNote("F",0.25,"saw")
+	# syn1.genNote("G",0.5,"saw")
 
-syn1.close()
